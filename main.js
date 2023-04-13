485ba504400957bf1df21354620367d504ec93c9
DarkApple55 <axelcy@gmail.com>
2023-04-13 19:08:17 -0300

    test anda

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-function test0() {
-    fetch("http://localhost:3001/user")
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
-}
+
 // http://www.omdbapi.com/?apikey=9141ea63
 // http://www.omdbapi.com/?apikey=9141ea63&i=tt3896198
 const test1 = async () => {
     try {
-        const response = await fetch("http://localhost:3001/user")
-        console.log(response)
+        const response = await fetch("http://www.omdbapi.com/?apikey=9141ea63&i=tt3896198")
+        const data = await response.json()
+        console.log(data)
     } catch (err) {
         console.log(err)
     }
