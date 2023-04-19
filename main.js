d6eb6ef481dd4112ba41599352483b0708fc3471
DarkApple55 <axelcy@gmail.com>
2023-04-19 09:00:19 -0300

    terminado v1

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 
-
+var count = 0
 const test3 = async () => {
+    
     try {
         // 'https://dummyjson.com/products?limit=10&skip=10&select=title,price'
         //  https://dummyjson.com/products/categories
@@ -13,8 +14,6 @@ const test3 = async () => {
         categories.map(async category => {
             const response = await fetch(`https://dummyjson.com/products/category/${category}`)
             const section = await response.json()
-            console.log(section.products)
-
             const container = document.createElement("section")
             document.getElementById("cards").appendChild(container)
 
@@ -28,7 +27,7 @@ const test3 = async () => {
             containerCards.setAttribute("id", `cards-container-${category}`)
             containerCards.setAttribute("class", "section-container-category")
 
-            document.getElementById("dropdown-categories").innerHTML += `<li><button class="dropdown-item" href="#" onclick="dropdownSelector(${category})">${title}</button></li>`
+            document.getElementById("dropdown-categories").innerHTML += `<li><button class="dropdown-item" href="#" onclick="dropdownSelector('${category}')">${title}</button></li>`
 
             section.products.map(product => {
                 let desc = product.description
@@ -36,7 +35,7 @@ const test3 = async () => {
                     desc = desc.substr(0, maxDescriptionLength) + desc.substr(maxDescriptionLength).split(" ")[0] + "..."
                 }
                 containerCards.innerHTML += `
-                <div class="card tarjeta">
+                <div class="card tarjeta" id="tarjeta-${product.title}">
                     <img class="card-img-top foto-card" src="${product.thumbnail}" alt="${product.title}">
                     <div class="card-body">
                         <p class="card-text"><b>${product.title}</b> - ${product.brand}</p>
@@ -45,26 +44,14 @@ const test3 = async () => {
                 </div>
                 `
             })
+            const leerinput = document.getElementById("search-bar")
+            leerinput.addEventListener("keyup", e => {
+                section.products.map(product => {
+                    if (!product.title.toUpperCase().includes(leerinput.value.toUpperCase())) document.getElementById(`tarjeta-${product.title}`).classList.add("d-none")
+                    else document.getElementById(`tarjeta-${product.title}`).classList.remove("d-none")
+                })
+            })
         })
-        
-        // const { products } = await response.json()
-        // console.log(products)
-        // 
-        // products.map(product => {
-        //     let desc = product.description
-        //     if (desc.length > maxDescriptionLength) {
-        //         desc = desc.substr(0, maxDescriptionLength) + desc.substr(maxDescriptionLength).split(" ")[0] + "..."
-        //     }
-        //     document.getElementById("cards-smartphones").innerHTML += `
-        //     <div class="card tarjeta">
-        //         <img class="card-img-top foto-card" src="${product.thumbnail}" alt="${product.title}">
-        //         <div class="card-body">
-        //             <p class="card-text"><b>${product.title}</b> - ${product.brand}</p>
-        //             <p class="card-text">${desc}</p>
-        //         </div>      
-        //     </div>
-        // `
-        // })
     } catch (err) {
         console.log(err)
     }
@@ -72,15 +59,14 @@ const test3 = async () => {
 test3()
 
 const dropdownSelector = category => {
-    document.getElementsByClassName("section-category").map(element => {
-        console.log(element)
-        // if(element.id != category) element.classList.add("d-none")
-    })
-
+    console.log(category)
+    const sections = document.getElementsByClassName("section-category")
+    for (let section of sections){
+        if (section.id != `cards-${category}`) section.classList.add("d-none")
+        else section.classList.remove("d-none")
+    }
 }
-
-/*
-document.getElementsByClassName("section-category").map(section => {
-        if (category != section.id) console.log(section.id)
-    })
-*/
+const resetFilter = () => {
+    const sections = document.getElementsByClassName("section-category")
+    for (let section of sections) section.classList.remove("d-none")
+}
